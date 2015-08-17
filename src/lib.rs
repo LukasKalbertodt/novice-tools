dfd3a3365130b2f67ed5867d5bf90fc333acd8ca
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2015-08-18 01:18:23 +0200

    Fix doc and provide doc link in Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,20 @@
 //! beginners. Especially reading values from standard input (the user) is
 //! very confusing for beginners.
 //!
+//! Currently all useful methods are in the `io` module.
+//!
 //! How to use this crate
 //! =====================
 //! First you need to put this into your `Cargo.toml`:
-//! ```
+//!
+//! ```ignore
 //! [dependencies]
 //! novice-tools = "*"
 //! ```
 //!
 //! Next you need to put this in your crate-root (usually `main.rs`):
-//! ```
+//!
+//! ```ignore
 //! extern crate novice_tools;
 //! use novice_tools::*;
 //! ```
